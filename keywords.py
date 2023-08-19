d5977286ccfb630c5e6e842f77f0522ffef1c2cc
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-08-19 13:10:09 -0500

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,33 +1,53 @@
 import streamlit as st
 import pandas as pd
-from collections import defaultdict
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-# Load the CSV file containing publication data (replace with your file path)
-csv_file_path = "abstract.csv"
-df = pd.read_csv(csv_file_path)
+# Load the dataset containing abstracts
+@st.cache
+def load_data():
+    return pd.read_csv('abstract.csv')  # Replace with the actual dataset path
 
-# Create a dictionary to store keyword co-occurrence frequencies
-keyword_cooccurrence = defaultdict(int)
+df = load_data()
 
-# Replace 'Title' with the actual column name containing keywords
-keywords_column_name = 'Title'
+# Text analysis and clustering app
+st.title('Abstract Text Analysis and Clustering')
 
-# Iterate through each row of the DataFrame
-for index, row in df.iterrows():
-    keywords = row[keywords_column_name].split(';')
-    keywords = [kw.strip().lower() for kw in keywords]  # Convert to lowercase
-    for kw1 in keywords:
-        for kw2 in keywords:
-            if kw1 != kw2:
-                keyword_pair = tuple(sorted([kw1, kw2]))
-                keyword_cooccurrence[keyword_pair] += 1
+# Sidebar to choose the number of topics
+num_topics = st.sidebar.slider('Select Number of Topics', 2, 10, 5)
 
-# Streamlit App Layout
-st.title("Keyword Co-occurrence Analysis")
-st.write("Analyzing co-occurrence of keywords in publications")
+# Preprocess the abstracts using CountVectorizer
+vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
+X = vectorizer.fit_transform(df['Abstract'])
 
-# Display Co-occurrence Results
-st.write("Keyword Co-occurrence Frequencies:")
-for pair, count in keyword_cooccurrence.items():
-    kw1, kw2 = pair
-    st.write(f"Keywords: {kw1}, {kw2} | Co-occurrence Count: {count}")
+# Apply LDA topic modeling
+lda_model = LatentDirichletAllocation(n_components=num_topics, random_state=42)
+lda_model.fit(X)
+
+# Display topics and their top words
+st.subheader('Topics and Top Words:')
+for idx, topic in enumerate(lda_model.components_):
+    st.write(f"Topic {idx + 1}:")
+    top_words = [vectorizer.get_feature_names()[i] for i in topic.argsort()[-10:]]
+    st.write(', '.join(top_words))
+
+# Visualize word cloud for each topic
+st.subheader('Word Clouds for Topics:')
+for idx, topic in enumerate(lda_model.components_):
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join([vectorizer.get_feature_names()[i] for i in topic.argsort()[-50:]]))
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot()
+
+# Cluster abstracts using topic probabilities
+topic_probabilities = lda_model.transform(X)
+df['Cluster'] = topic_probabilities.argmax(axis=1)
+
+# Display clustered abstracts
+st.subheader('Clustered Abstracts:')
+selected_cluster = st.selectbox('Select Cluster', range(num_topics))
+clustered_abstracts = df[df['Cluster'] == selected_cluster]['Abstract'].tolist()
+st.write(clustered_abstracts)
