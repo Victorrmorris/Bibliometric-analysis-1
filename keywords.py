3355ac4a3604243058fab784a025ac3495f4dafe
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-08-19 11:13:17 -0500

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,15 +1,33 @@
 import streamlit as st
 import pandas as pd
+from collections import defaultdict
 
-# Load the CSV file
+# Load the CSV file containing publication data (replace with your file path)
 csv_file_path = "scopus.csv"
 df = pd.read_csv(csv_file_path)
 
-# Streamlit App Layout
-st.title("Simple Streamlit App")
-st.write("Displaying Basic Information")
+# Create a dictionary to store keyword co-occurrence frequencies
+keyword_cooccurrence = defaultdict(int)
 
-# Display DataFrame info
-st.write("Number of Rows:", len(df))
-st.write("First Few Rows:")
-st.write(df.head())
+# Replace 'Title' with the actual column name containing keywords
+keywords_column_name = 'Title'
+
+# Iterate through each row of the DataFrame
+for index, row in df.iterrows():
+    keywords = row[keywords_column_name].split(';')
+    keywords = [kw.strip().lower() for kw in keywords]  # Convert to lowercase
+    for kw1 in keywords:
+        for kw2 in keywords:
+            if kw1 != kw2:
+                keyword_pair = tuple(sorted([kw1, kw2]))
+                keyword_cooccurrence[keyword_pair] += 1
+
+# Streamlit App Layout
+st.title("Keyword Co-occurrence Analysis")
+st.write("Analyzing co-occurrence of keywords in publications")
+
+# Display Co-occurrence Results
+st.write("Keyword Co-occurrence Frequencies:")
+for pair, count in keyword_cooccurrence.items():
+    kw1, kw2 = pair
+    st.write(f"Keywords: {kw1}, {kw2} | Co-occurrence Count: {count}")
